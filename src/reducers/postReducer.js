917cef8dfbda5e1788c506ff2a3603d70d55b690
c.p.kondas <c.p.kondas@leidos.com>
2020-07-02 08:35:01 -0400

    Add NEW_POST action

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -11,7 +11,12 @@ export default function(state = initialState, action) {
             return {
                 ...state,
                 items: action.payload
-            }
+            };
+        case NEW_POST:
+            return {
+                ...state,
+                item: action.payload
+            };
         default:
             return state;
     }
